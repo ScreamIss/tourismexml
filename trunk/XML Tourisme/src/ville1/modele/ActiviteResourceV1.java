3d5805536b896227b3e849060acb4357d3557dbb
guillaume.b7 <guillaume.b7@26e307ae-4c53-4502-eb89-116937b7fe75>
2010-06-05 13:21:04 +0000

    GBR : création des ressource et des serveurs ressources

---
+++ trunk/XML Tourisme/src/ville1/modele/ActiviteResourceV1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ville1.modele;
+
+/**
+ *
+ * @author Guillaume
+ */
+public class ActiviteResourceV1 {
+
+}
